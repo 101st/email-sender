6721b5fd25abf1a5cc5b8edeb287feb51557f887
potorochin <3dspaun@gmail.com>
2019-08-23 15:53:42 +0300

    upd

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
 
-var indexRouter = require('./routes/index');
+const indexRouter = require('./routes/index');
+const config = require('./config');
+const api = require('./api/api');
+const service = require('./service/service');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.dev.mongodb, { useNewUrlParser: true }, function () {
+  console.log('Mongodb connected');
+  //service.startQueue();
+});
 
 var app = express();
 
@@ -19,14 +29,15 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.post('/api', api.addEmailsToDatabase);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
---
+++ config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  dev: {
+    mongodb: 'mongodb://potorochinau:acera510@ds139937.mlab.com:39937/ns2',
+    bitrix: {
+      host: 'run24.bitrix24.ru',
+      user_id: '1513',
+      webhook_token: 'v5prxf1c1gdyya56',
+      auth_type: true
+    }
+  },
+  prod: {
+
+  }
+};
